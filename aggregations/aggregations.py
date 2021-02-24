aa761991a14b711334aee8b0e98c508349ef872f
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-02-23 21:31:10 -0300

    making method generic

--- aggregations/aggregations.py
+++ aggregations/aggregations.py
@@ -1,17 +1,12 @@
-import re
 from pymongo import MongoClient
 import pprint
 
 client = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
 
 
-def match():
+def run(pipeline):
     result = client['comic-book']['DC'].aggregate([
-        {
-            '$match': {
-                'name': re.compile(r"Batman")
-            }
-        }
+        pipeline
     ])
 
     pprint.pprint(list(result))
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from aggregations.aggregations import match
+import re
+
+from aggregations.aggregations import run
 
 
 def main():
-    match()
+    run({
+        '$match': {
+            'name': re.compile(r"Flash")
+        }
+    })
 
 
 if __name__ == "__main__":
