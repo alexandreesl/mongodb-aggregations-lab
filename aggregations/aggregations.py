45bf3ed8b4fe83310f68d0a9bc2ed375bc75d0c7
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-02-23 22:47:31 -0300

    first aggregation

--- aggregations/aggregations.py
+++ aggregations/aggregations.py
@@ -5,8 +5,8 @@ client = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=
 
 
 def run(pipeline):
-    result = client['comic-book']['DC'].aggregate([
+    result = client['comic-book']['DC'].aggregate(
         pipeline
-    ])
+    )
 
     pprint.pprint(list(result))
--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from aggregations.aggregations import run
 
 
 def main():
-    run({
+    run([{
         '$match': {
-            'name': re.compile(r"Flash")
+            'name': re.compile(r"Flash ", flags=re.IGNORECASE),
+            'ALIGN': 'Good Characters'
         }
-    })
+    },
+    {
+        '$project': {
+            'FIRST APPEARANCE': 1, 'name': 1, '_id': 0
+        }
+    }])
+
 
 
 if __name__ == "__main__":
