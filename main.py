1acbcff17690a99e4895b896235ef1365b1276bb
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2021-03-08 20:15:25 -0300

    more aggregations

--- main.py
+++ main.py
@@ -124,6 +124,69 @@ def main():
             }
         }
     ])
+    print('------------------------------------------------------')
+    run('DC', [
+        {
+            '$match': {
+                'YEAR': {'$exists': True}
+            }
+        },
+        {
+            '$bucketAuto': {
+                'groupBy': '$YEAR',
+                'buckets': 10,
+                'granularity': 'E192',
+                'output': {
+                    'count': {'$sum': 1}
+                }
+            }
+        }
+    ])
+    print('------------------------------------------------------')
+    run('DC', [
+        {
+            '$facet': {
+                'Silver age characters': [
+                    {
+                        '$match': {
+                            'FIRST APPEARANCE': {'$exists': True}
+                        }
+                    },
+                    {
+                        '$project': {
+                            '_id': 0,
+                            'name': 1,
+                            'year': {
+                                '$toInt': {'$arrayElemAt': [{'$split': [{'$toString': '$FIRST APPEARANCE'}, ","]}, 0]}}
+                        }
+                    },
+                    {
+                        '$match': {
+                            'year': {"$gte": 1956, "$lte": 1970}
+                        }
+                    },
+                    {
+                        '$sort': {
+                            'name': 1
+                        }
+                    }
+                ],
+                'Characters by decade': [
+                    {
+                        '$bucket': {
+                            'groupBy': "$YEAR",
+                            'boundaries': [1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020],
+                            'default': "Unknown",
+                            'output': {
+                                'count': {'$sum': 1}
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {'$out': 'DC-reports'}
+    ])
 
 
 if __name__ == "__main__":
